7613e41106c3d941251eda4a85313254a07f0f31
wiwin.ristanto <wiwinristanto94@gmail.com>
2022-01-06 11:19:52 +0700

    dev : Membuat Layout Hitung Luas Lingkarang

---
+++ src/Lingkaran.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Lingkaran {
+    private JPanel LingkaranPanel;
+    private JButton hitungButton;
+    private JButton batalButton;
+    private JButton keluarButton;
+    private JTextField textField1;
+    private JTextField textField2;
+}
